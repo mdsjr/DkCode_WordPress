ccc91b07d5ca822645806b8d3455c958f24d2a96
Moacir Domingos <mds.junior87@gmail.com>
2023-06-29 21:25:56 -0300

    wordpress

---
+++ danki/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Danki Code - Tema</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+	<<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap" rel="stylesheet">
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+	<section class="topo">
+		<div class="center">
+			<header>
+				<div class="logo"><img src="images/logo.png"> </div>
+			</header>
+			<ul class="menu-desktop">
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+			<div class="clear"></div>
+			
+		</div><!--center-->
+	</section><!--topo-->
+
+</body>
+</html>
---
+++ teste.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+
+<div id="en">
+<body>
+
+<?php
+	namespace 
+	
+	class ClassName
+	{
+	    
+	}
+	
+
+?>
+
+
+	<nav></nav>
+	<div></div>
+
+	<div id="id"></div>
+
+</body>
+</html>
