c5fbbed89a15b13065ac74da274cb77edd85ac8e
Moacir Domingos <mds.junior87@gmail.com>
2023-06-30 16:48:27 -0300

    30.06.2023

--- danki/index.php
+++ danki/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-	
 	<title>Danki Code - Tema</title>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
 	<<link rel="preconnect" href="https://fonts.googleapis.com">
@@ -10,11 +9,10 @@
 	<link href="style.css" rel="stylesheet">
 </head>
 <body>
-
 	<section class="topo">
 		<div class="center">
 			<header>
-				<div class="logo"><img src="images/logo.png"> </div>
+				<div class="logo"><img src="images/logo.png"> </div><!--logo-->
 			</header>
 			<ul class="menu-desktop">
 				<li><a href="">Home</a></li>
@@ -22,9 +20,18 @@
 				<li><a href="">Contato</a></li>
 			</ul>
 			<div class="clear"></div>
-			
+			<br />
+			<br />
+			<div class="w50 time-descricao">
+				<h2>Melhore a comunicação com seu cliente e time.</h2>
+				<p>Consultoria especializada em startups, empresas e principalmente pessoas</p>
+				<a target="_blank" href="http://dankicode.com">Ver demonstração</a>
+			</div><!--w50-->
+			<div class="w50 time-imagem">
+				<img src="images/ilustracao-equipe.png">  
+			</div><!--w50-->
+			<div class="clear"></div>
 		</div><!--center-->
 	</section><!--topo-->
-
 </body>
 </html>
